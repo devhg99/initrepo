f9153c448fd6827bf9c6a5c2d5d6703bbf2fa00b
devhg99 <et3015@naver.com>
2025-07-07 15:49:42 +0900

    브랜치 변경 테스트2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React start
+          Learn React start team2
         </a>
       </header>
     </div>
